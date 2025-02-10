bc894c088bad2d732460b99a8b4ba3bbb8a3ce5c
atanunu <47770330+atanunu@users.noreply.github.com>
2025-02-10 13:50:18 +0100

    Create RequestTypeErrorMessages.php

---
+++ System/config/RequestTypeErrorMessages.php
@@ -0,0 +1,7 @@
+<?php
+// config/RequestTypeErrorMessages.php
+
+return [
+    'invalid_request_method' => 'Invalid request method. Expected :expected but received :actual.',
+    'unauthorized_ip'        => 'Unauthorized IP address: :ip is not allowed.',
+];
